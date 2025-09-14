306992025f83f4a00973a5f2e55408276846e83c
bcajales <bernardocajales@gmail.com>
2025-09-14 00:56:52 -0300

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // Archivo: deps.ts
 export { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-export { default as puppeteer } from "https://deno.land/x/puppeteer@22.10.0/mod.ts";
+export { chromium } from "https://deno.land/x/playwright@v1.44.0/mod.ts"; // <-- CAMBIO PRINCIPAL
 export * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
