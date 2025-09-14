0dec1d91ba78126a8b76f41c66abf4f97e771107
bcajales <bernardocajales@gmail.com>
2025-09-14 00:38:43 -0300

    Create deps.ts

---
+++ deps.ts
@@ -0,0 +1,4 @@
+// Archivo: deps.ts
+export { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+export { default as puppeteer } from "https://deno.land/x/puppeteer@22.10.0/mod.ts";
+export * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
